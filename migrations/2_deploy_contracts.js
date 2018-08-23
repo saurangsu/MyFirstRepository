1395c9237d17defba5b83100792d61ec4b02671c
saurangsu <35784115+saurangsu@users.noreply.github.com>
2018-08-23 15:47:41 -0400

    Web Page
    Connected the smart contract to a web page - at a very basic level. Many more changes are required

---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,5 @@
+var bet = artifacts.require("bet");
+
+module.exports = function(deployer) {
+  deployer.deploy(bet);
+};
---
+++ src/js/app.js
@@ -0,0 +1,94 @@
+App = {
+  web3Provider: null,
+  contracts: {},
+  names: new Array(),
+  url: 'http://127.0.0.1:9545',
+  chairPerson:null,
+  currentAccount:null,
+  init: function() {
+     return App.initWeb3();
+  },
+
+  initWeb3: function() {
+        // Is there is an injected web3 instance?
+    if (typeof web3 !== 'undefined') {
+      App.web3Provider = web3.currentProvider;
+    } else {
+      // If no injected web3 instance is detected, fallback to the TestRPC
+      App.web3Provider = new Web3.providers.HttpProvider(App.url);
+    }
+    web3 = new Web3(App.web3Provider);
+
+    App.populateAddress();
+    return App.initContract();
+  },
+
+  initContract: function() {
+      $.getJSON('bet.json', function(data) {
+    // Get the necessary contract artifact file and instantiate it with truffle-contract
+    var betArtifact = data;
+    App.contracts.bet = TruffleContract(betArtifact);
+
+    // Set the provider for our contract
+    App.contracts.bet.setProvider(App.web3Provider);
+    
+   
+    return App.bindEvents();
+  });
+  },
+
+  bindEvents: function() {
+    $(document).on('click', '#register', function(){ var ad = $('#registerAddress').val(); App.handleRegister(ad);   });
+  },
+
+  populateAddress : function(){
+    new Web3(new Web3.providers.HttpProvider(App.url)).eth.getAccounts((err, accounts) => {
+      jQuery.each(accounts,function(i){
+        if(web3.eth.coinbase != accounts[i]){
+          var optionElement = '<option value="'+accounts[i]+'">'+accounts[i]+'</option';
+          jQuery('#registerAddress').append(optionElement);  
+        }
+      });
+    });
+  },
+
+ 
+
+  handleRegister: function(addr){
+
+    var betInstance;
+    App.contracts.bet.deployed().then(function(instance) {
+      betInstance = instance;
+      return betInstance.register(addr);
+    }).then( function(result){
+      if(result.receipt.status == '0x01')
+        alert(addr + " is registered successfully")
+      else
+        alert(addr + " account registeration failed due to revert")
+    }).catch( function(err){
+      alert(addr + " account registeration failed")
+    })
+  },
+
+
+
+  handleWinner : function() {
+    var voteInstance;
+    App.contracts.vote.deployed().then(function(instance) {
+      voteInstance = instance;
+      return voteInstance.winningProposal();
+    }).then(function(res){
+      alert(App.names[res] + "  is the winner ! :)");
+    }).catch(function(err){
+      console.log(err.message);
+    })
+  }
+};
+
+
+$(function() {
+  $(window).load(function() {
+    App.init();
+    console.log('starting app.js');
+  });
+});
